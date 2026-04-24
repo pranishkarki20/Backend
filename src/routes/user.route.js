997c7ae91af236945f527bea1ac618a0c5d1271b
pranishkarki20 <praniskkarki20@gmail.com>
2026-04-24 21:22:00 +0530

    update

--- src/routes/user.route.js
+++ src/routes/user.route.js
@@ -1,8 +1,13 @@
 import {Router} from 'express';
 import { loginUser, logoutUser, registerUser } from '../controllers/user.controller.js';
  
-const router = Router() ; 
-router.route('/register').post(registerUser)
+const router = Router() ; //Creating  router instance 
+//router.route means define  multiple Http  methods for the 
+// same URL path in one chain.
+
+
+//IN short router means  group all request methods for the same  path together 
+router.route('/register').post(registerUser) // Create a backend POST API route /register that runs the registerUser function.
 router.route('/login').post(loginUser);
 router.route('/logout').post(logoutUser);
 export default router ; 
