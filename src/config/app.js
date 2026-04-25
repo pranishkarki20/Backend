def4fb1a2ace951513337bc90361df438b1edc70
pranishkarki20 <praniskkarki20@gmail.com>
2026-04-25 21:44:59 +0530

    cores

--- src/config/app.js
+++ src/config/app.js
@@ -1,7 +1,9 @@
 import express from "express" ;
+import cors from "cors";
 
 const app = express(); 
 
+app.use(cors());
 app.use(express.json()); //expected it to be in json format 
  
 import userRouter from '../routes/user.route.js';
