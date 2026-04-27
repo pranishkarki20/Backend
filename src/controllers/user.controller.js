962f1d9bbe6657d221a40523507d1ca1ae7ab6b8
pranishkarki20 <praniskkarki20@gmail.com>
2026-04-27 23:21:19 +0530

    update

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -3,12 +3,6 @@ import { User } from "../models/user.model.js";
 const registerUser = async (req, res) => {
     try {
         const { username, email, password } = req.body;
-
-        // basic validation
-        if (!username || !email || !password) {
-            return res.status(400).json({ message: "All fields are required" });
-        }
-
         const existing = await User.findOne({ email: email.toLowerCase() });
         if (existing) {
             return res.status(400).json({ message: "User already exists" });
---
+++ src/middleware/registermiddle.js
@@ -0,0 +1,10 @@
+const validregister = (req, res,next) =>{
+    const {username , email , password} = req.body ; 
+    if(!username || !email || !password){
+        return res.status(400).json({message: "All fields are required"});
+    }
+    next();
+};
+export{
+    validregister
+}
