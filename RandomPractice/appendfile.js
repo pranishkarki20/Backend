5bcddb26ad0aa776fee829d142ab99ca954286d8
pranishkarki20 <praniskkarki20@gmail.com>
2026-05-01 21:32:44 +0530

    Update

--- RandomPractice/appendfile.js
+++ RandomPractice/appendfile.js
@@ -1,5 +1,13 @@
 import fs from "fs" 
 
+//synchronous 
+console.log("start");
+fs.appendFileSync("Pranish.txt", '\nHello Guys ! Welcome to my Video');
+const data =fs.readFileSync("Pranish.txt" ,'utf-8')
+console.log(data);
+console.log("End");
+
+//asynchronous 
 fs.appendFile('Pranish.txt','\nNew Line' , (err) => {
     if(err) throw err ; 
     console.log('Data appended');
